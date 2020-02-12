a0a5189969032d2b766b26c76e6a81979c2d044d
lmxyjy <yjy15680489038@163.com>
2020-02-12 16:29:34 +0800

    函数节流

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# learn-javaScript
-javaScript中常用的函数解决方案
+**javaScript中常用的函数解决方案**
+
+[**一、函数防抖和函数节流**](https://lmxyjy.github.io/%E9%98%B2%E6%8A%96%E5%92%8C%E8%8A%82%E6%B5%81/)
+
+
--- 节流/throttle.js
+++ 节流/throttle.js
@@ -1,10 +1,18 @@
-//关于节流的实现，有两种主流的实现方式，一种是使用时间戳，一种是设置定时器。
+
 const container = document.getElementById("container");
 let conut = 1;
 function getUserAction(){
     container.innerHTML = conut++;
 }
-//使用时间戳 ---第一次可以默认执行
+
+/*关于节流的实现，有两种主流的实现方式，一种是使用时间戳，一种是设置定时器。*/
+
+/**
+ * 示例一
+ * 使用时间戳的方式，实现节流---第一次可以默认执行
+ * @param {Function} fn 
+ * @param {Number} wait 
+ */
 function throttle1(fn,wait){
     let oldTime = 0;
     return function(...args){
@@ -16,7 +24,12 @@ function throttle1(fn,wait){
     }
 }
 
-//使用定时器 --- 离开之后同样可以执行
+/**
+ * 示例二
+ * 使用定时器的方式 --- 离开之后可以执行
+ * @param {Function} fn 
+ * @param {Number} wait 
+ */
 function throttle2(fn ,wait){
     let timeId;
     return function(...args){
@@ -29,114 +42,86 @@ function throttle2(fn ,wait){
     }
 }
 
-// container.onmousemove = throttle2(getUserAction,1000);
-
-function throttle3(func,wait){
-    var timeId,oldTime=0;
+/**
+ * 示例三
+ * 1. 第一次默认立即触发。
+ * 2. 并且结束了以后再触发一次。
+ * @param {Function} fn 
+ * @param {Number} wait 
+ */
+function throttle3(fn,wait){
+    let timeId,oldTime=0;
 
     return function(...args){
-        var nowTime = +Date.now();
-        var remainder = wait - (nowTime - oldTime);
-        //第一次可以默认执行
-        if(remainder <= 0){ //说明到时间了，需要执行了
+        let nowTime = +Date.now();
+        let remaining = wait - (nowTime - oldTime);
+        if(remaining <= 0){ 
+            //如果进来以后发现有一个定时器存在,为了避免执行2次.清除掉这个定时器,并且执行函数.
             if(timeId){
-                clearTimeout(timeId)
+                clearTimeout(timeId);
                 timeId = null;
             }
             oldTime = nowTime;
-            func.apply(this,args);
-        }else if(!timeId){ //如果在时间周期中触发，则执行这个语句块
-            timeId = setTimeout(() => {
-                oldTime = +Date.now();
+            fn.apply(this,args);
+        }else if(!timeId){
+            //如果是在中途触发了这个函数,那么根据计算出的剩余执行时间.在剩余执行时间达到后调用定时器执行一次fn
+            timeId = setTimeout(()=>{
+                oldTime = nowTime;
                 timeId = null;
-                func.apply(this,args);
-            }, remainder);
+                fn.apply(this,args);
+            },remaining) 
         }
     }
 }
 
-function throttle4(func,wait,options){
-    var timeout,context;
-    var previous = 0;
-    !options && (options = {}); //如果没有传值，则给一个空对象
-    
-    var throttled = function(...args){
-        var now = +Date.now();
-        (!previous && options.leading === false) && (previous = now); //说明不需要首次执行
-        var remaining = wait - (now - previous);
-        context = this;
-        if(remaining <= 0 || remaining > wait){
-            if(timeout){
-                clearTimeout(timeout);
-                timeout = null;
+/**
+ * 示例四
+ * 可以通过传入参数决定头尾的有无。
+ * leading：false 表示无头有尾
+ * trailing: false 表示有头无尾
+ * 注意：不能同时设置2个值都为false
+ * @param {Function} fn 
+ * @param {Number} wait 
+ * @param {leading:Boolean,trailing:Boolean} options 
+ */
+function throttle4(fn,wait,options){
+    let timeId,oldTime=0;
+    !options && (options = {}); //如果options没有传递，那么就定义为一个空对象
+
+    return function(...args){
+        let nowTime = +Date.now();
+        !oldTime && options.leading === false && (oldTime = nowTime); //第一次不执行
+        let remaining = wait - (nowTime - oldTime);
+        
+        if(remaining <= 0){ 
+            //如果进来以后发现有一个定时器存在,为了避免执行2次.清除掉这个定时器,并且执行函数.
+            if(timeId){
+                clearTimeout(timeId);
+                timeId = null;
             }
-            previous = now;
-            func.apply(context,args);
-        }else if(!timeout && options.trailing !== false){
-            timeout = setTimeout(()=>{
-                previous = options.leading === false ? 0 : +Date.now();
-                timeout = null;
-                func.apply(context,args);
-            },remaining)
+            oldTime = nowTime;
+            fn.apply(this,args);
+        }else if(!timeId && options.trailing !== false){
+            //如果是在中途触发了这个函数,那么根据计算出的剩余执行时间.在剩余执行时间达到后调用定时器执行一次fn
+            timeId = setTimeout(()=>{
+                oldTime = nowTime;
+                timeId = null;
+                fn.apply(this,args);
+            },remaining) 
         }
     }
-    return throttled
 }
 
-// container.onmousemove = throttle3(getUserAction,1000);
-
-// function throttle(fn,wait){
-//     let oldTime=0;
-//     return function(...args){
-//         let nowTime = +Date.now();
-//         let remaining = wait - (nowTime - oldTime); //计算出触发的剩余时间
-//         if(remaining <= 0){ //到了时间才执行
-//             oldTime = nowTime;
-//             fn.apply(this,args);
-//         }
-//     }
-// }
-
-// function throttle(fn,wait){
-//     let timeId;
-//     return function(...args){
-//         !timeId && (
-//             timeId = setTimeout(()=>{
-//                 fn.apply(this,args)
-//                 timeId = null; //到了wait时间后才置为null,下一次定时器才得以执行
-//             },wait)
-//         )        
-//     }
-// }
-
-// function throttle(fn,wait,options){
-//     let timeId,oldTime=0;
-//     !options && (options = {}); //如果options没有传递，那么就定义为一个空对象
-
-//     return function(...args){
-//         let nowTime = +Date.now(); //得到当前的时间
-//         !oldTime && options.leading === false && (oldTime = nowTime); //如果满足条件则第一次不执行
-//         let remaining = wait - (nowTime - oldTime); //计算出剩余的时间
-//         action = true;
-//         if(remaining <= 0){ 
-//             //如果进来以后发现有一个定时器存在,为了避免执行2次.清除掉这个定时器,并且执行函数.
-//             if(timeId){
-//                 clearTimeout(timeId);
-//                 timeId = null;
-//             }
-//             oldTime = nowTime;
-//             fn.apply(this,args);
-//         }else if(!timeId && options.trailing !== false){
-//             //如果是在中途触发了这个函数,那么根据计算出的剩余执行时间.在剩余执行时间达到后调用定时器执行一次fn
-//             timeId = setTimeout(()=>{
-//                 oldTime = nowTime;
-//                 timeId = null;
-//                 fn.apply(this,args);
-//             },remaining) 
-//         }
-//     }
-// }
-
+/**
+ * 示例五
+ * 封装的节流的类，throttle函数可以实现节流，cancel函数可以实现取消
+ * leading：false 表示无头有尾
+ * trailing: false 表示有头无尾
+ * 注意：不能同时设置2个值都为false
+ * @param {Function} fn 
+ * @param {Number} wait 
+ * @param {leading:Boolean,trailing:Boolean} options 
+ */
 class Throttled{
     constructor(){
         this.timeId = null; //记录定时器id
@@ -178,7 +163,9 @@ const options = {
     trailing:false
 }
 
+/*实例化对象*/
 const t = new Throttled();
 
+/*调用的时候注意绑定this的指向*/
 container.onmousemove = t.throttle(getUserAction,10000,options).bind(t);
 document.getElementById("btn").onclick = t.cancel.bind(t);
