0a61dd6cfa055234d050a63f217b96e607e2bec3
lmxyjy <yjy15680489038@163.com>
2020-04-08 18:07:00 +0800

    性能对比

--- jquery中each方法的实现/each.js
+++ jquery中each方法的实现/each.js
@@ -158,20 +158,88 @@ function each3(obj,callback){
     }
 }
 
-each3(testArr,function(i,value){
-    console.log(`this==${this} i==${i} value==${value}`);
-    /**
-     * this==1 i==0 value==1
-     * this==2 i==1 value==2
-     * this==3 i==2 value==3
-     */
-})
+// each3(testArr,function(i,value){
+//     console.log(`this==${this} i==${i} value==${value}`);
+//     /**
+//      * this==1 i==0 value==1
+//      * this==2 i==1 value==2
+//      * this==3 i==2 value==3
+//      */
+// })
 
-each3(testObj,function(key,value){
-    console.log(`this==${this} key==${key} value==${value}`);
-    /**
-     * this==name key==0 value==name
-     * this==age key==1 value==age  
-     * this==sex key==2 value==sex
-     */
+// each3(testObj,function(key,value){
+//     console.log(`this==${this} key==${key} value==${value}`);
+//     /**
+//      * this==name key==0 value==name
+//      * this==age key==1 value==age  
+//      * this==sex key==2 value==sex
+//      */
+// })
+
+//关于for和each函数的速度比较
+var arr = Array.from({length:1000000},(v,i)=>i);
+
+console.time('for');
+var i = 0;
+for(;i<arr.length;i++){
+    i += arr[i]
+}
+console.timeEnd('for'); 
+
+console.time("each")
+var j = 0;
+each3(arr,function(i,value){
+    j += value;
 })
+console.timeEnd("each")
+
+//for: 0.122ms
+//each: 31.453ms
+
+// 可以看出，for 循环的性能是明显好于 each 函数的，each 函数本质上也是用的 for 循环，到底是慢在了哪里呢？
+
+//不使用call
+function each(obj, callback) {
+    var i = 0;
+    var length = obj.length
+    for (; i < length; i++) {
+        value = callback(i, obj[i]);
+    }
+}
+
+//使用call绑定this
+function eachWithCall(obj, callback) {
+    var i = 0;
+    var length = obj.length
+    for (; i < length; i++) {
+        value = callback.call(obj[i], i, obj[i]);
+    }
+}
+
+var arr = Array.from({length: 1000000}, (v, i) => i);
+
+console.time('each')
+var i = 0;
+each(arr, function(index, item){
+    i += item;
+})
+console.timeEnd('each')
+
+
+console.time('eachWithCall')
+var j = 0;
+eachWithCall(arr, function(index, item){
+    j += item;
+})
+console.timeEnd('eachWithCall')
+
+//each: 19.640ms
+//eachWithCall: 27.211ms
+
+ /**
+ * each 函数和 eachWithCall 函数唯一的区别就是 eachWithCall 调用了 call，
+ * 从结果我们可以推测出，call 会导致性能损失，但也正是 call 的存在，
+ * 我们才能将 this 指向循环中当前的元素。
+  */
+
+//结束
