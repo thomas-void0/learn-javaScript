2f47c772aaef71ec81285b0f271ae4b5eb9c688c
lmxyjy <yjy15680489038@163.com>
2020-04-09 09:35:26 +0800

    对比+0和-0

--- 参数相等的判断/equal.js
+++ 参数相等的判断/equal.js
@@ -2,3 +2,15 @@ console.log(+0 === -0); //true
 console.log(NaN === NaN); //false
 console.log((+0).toString()); //'0'
 console.log((-0).toString());//'0'
+
+console.log(1/+0); //Infinity
+console.log(1/-0); //-Infinity
+
+// 对于正负零的判断
+function eq(a,b){
+    if(a === b) return a !== 0 || 1/a === 1/b;
+    return false; //其他情况下return false
+}
+
+console.log(eq(0,0)); //true
+console.log(eq(+0,-0)); //false
