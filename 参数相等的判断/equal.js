6d12398d6f3fc944c4209cf4832a4172832ef16c
lmxyjy <yjy15680489038@163.com>
2020-04-09 09:49:00 +0800

    函数第一版完成

--- 参数相等的判断/equal.js
+++ 参数相等的判断/equal.js
@@ -14,3 +14,34 @@ function eq(a,b){
 
 console.log(eq(0,0)); //true
 console.log(eq(+0,-0)); //false
+
+//对比NaN
+function eq2(a,b){
+    if(a !== a) return b !== b;
+}
+
+console.log(eq2(NaN,NaN));//true
+
+/**
+ * 第一版参数对比函数,用来过滤掉简单的类型比较，复杂的对象使用deepEq函数进行处理
+ * @param {Any} a 
+ * @param {Any} b 
+ */
+function eqFirst(a,b){
+    //结果为true区分出+0和-0
+    if(a === b) return a !== 0 || 1/a !== 1/b;
+
+    //typeof null的结果为object，这里判断，是为了让有null的情况尽早退出函数
+    if(a == null || a == null) return false;
+
+    //判断NaN
+    if(a !== a) return b !== b;
+
+    // 判断参数a类型，如果是基本类型，在这里可以直接返回false
+    var type = typeof a;
+    if(type !== 'function' && type !== 'object' && typeof b !== 'object') return false;
+
+    //更复杂的对象使用deepEq函数进行判断
+    return deepEq(a,b);
+
+}
