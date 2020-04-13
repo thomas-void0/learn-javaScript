f7f0ec9d5dfe7c277785ee037fb1927019a89bc1
lmxyjy <yjy15680489038@163.com>
2020-04-13 14:39:12 +0800

    柯里化第二版本

--- 函数柯里化/curry.js
+++ 函数柯里化/curry.js
@@ -29,5 +29,23 @@ addCurry(1,2)
  * @param {Function} fn 
  */
 function sub_curry(fn){
+    //截取到除了第一个函数参数以外的其他参数
+    var args = [].slice.call(arguments,1);
+    return function(){
+        var newArgs = args.concat([].slice.call(arguments));//得到后面传入的参数 
+        return fn.apply(this,newArgs)
+    }
+}
 
+function curry(fn,length){
+    length = length || fn.length; //表示传入函数的参数个数
+    var slice = Array.prototype.slice;
+    return function(){
+        if(arguments.length < length){
+            var combined = [fn].concat(slice.call(arguments));
+            return curry(sub_curry.apply(this,combined),length - arguments.length);
+        }else{
+            return fn.apply(this,arguments);
+        }
+    }
 }
