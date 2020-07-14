df2b5c116f5c8699499675ce9b4eda6a48c1a39d
newrankyangjingyu <yangjingyu@newrank.cn>
2020-07-14 16:26:17 +0800

    splice的实现

---
+++ splice的实现/splice.js
@@ -0,0 +1,108 @@
+"use strict";
+/*
+ * @Author: your name
+ * @Date: 2020-07-13 20:01:05
+ * @LastEditTime: 2020-07-14 16:13:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \_webpack-based:\code\learn-javaScript\splice的实现\splice.ts
+ */
+Array.prototype.splice = function (start, deleteCount) {
+    var argumentsLen = arguments.length; //得到传入的参数长度
+    var array = Object(this); //得到调用splice方法的数组
+    var len = array.length; //得到调用splice方法的数组的长度
+    var deleteArr = new Array(deleteCount); //创建一个存储删除元素的数组
+    var addElements = Array.prototype.slice.call(arguments, 2); //获取到需要添加的元素
+    // 拷贝删除的元素
+    sliceDeleteElements(array, start, deleteCount, deleteArr);
+    //对元素进行位移操作
+    if (addElements.length > 0 && deleteCount) {
+        movePostElements(array, start, len, deleteCount, addElements);
+    }
+    // 对源数组进行替换操作
+    for (var i = 0; i < addElements.length; i++) {
+        array[start + i] = addElements[i];
+    }
+    return deleteArr; //返回被删除的项目
+};
+// 让deleteArr数组中存储需要被删除的元素
+function sliceDeleteElements(array, start, deleteCount, deleteArr) {
+    if (deleteCount) {
+        for (var i = 0; i < deleteCount; i++) {
+            var index = start + i;
+            if (index in array) {
+                //获取被删除的元素
+                var current = array[index];
+                // 将被删除的元素添加到存储数组中
+                deleteArr[i] = current;
+            }
+        }
+    }
+    else {
+        for (var i = 0; i < array.length - start; i++) {
+            var index = start + i;
+            if (index in array) {
+                deleteArr[i] = array[index];
+            }
+        }
+    }
+}
+// 对数组元素进行位移操作
+function movePostElements(array, start, len, deleteCount, addElements) {
+    var addLen = addElements.length;
+    // 1，删除数量和替换数量一致,就不需要进行位移操作，直接赋值即可
+    if (deleteCount === addLen)
+        return;
+    // 2，删除数量大于替换数量
+    if (deleteCount > addLen) {
+        // 从被删除的最后一个元素开始遍历
+        for (var i = start + deleteCount; i < len; i++) {
+            // 得到要被移动的元素的索引值
+            var fromIndex = i;
+            // 要被移动到的新位置的索引值
+            var toIndex = i - (deleteCount - addLen);
+            // 将原本位置上的元素进行覆盖
+            if (fromIndex in array) {
+                array[toIndex] = array[fromIndex];
+            }
+            else {
+                delete array[toIndex];
+            }
+        }
+        // 去除掉多余长度的元素，目前应该的长度是 len + addLen - deleteCount,
+        // 把超过这个长度的数组元素都去除掉
+        for (var i = len - 1; i >= len + addLen - deleteCount; i--) {
+            delete array[i];
+        }
+    }
+    // 3，删除数量小于替换数量
+    if (deleteCount < addLen) {
+        // 如果删除的元素少于添加的元素，那么后面的元素向后移动addLen - deleteCount位
+        // 遍历需要被位移的元素
+        for (var i = len - 1; i >= start + deleteCount; i--) {
+            // 需要被往后挪动的元素位置索引
+            var fromIndex = i;
+            // 需要被挪动到的位置，这里是倒序的。
+            var toIndex = i + (addLen - deleteCount);
+            if (fromIndex in array) {
+                array[toIndex] = array[fromIndex];
+            }
+            else {
+                delete array[toIndex];
+            }
+        }
+    }
+}
+// 边界情况处理
+//对start索引值的处理
+function computeStartIndex(start, len) {
+    // 处理索引负数的情况,从反方向取索引值
+    if (start < 0) {
+        return start + len > 0 ? start + len : 0;
+    }
+    // 如果大于数组长度情况下
+    return start >= len ? len : start;
+}
+//对deleteCount删除个数的处理
+function computeDeleteCount(start) {
+}
