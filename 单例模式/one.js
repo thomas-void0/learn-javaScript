07e7b8452cdff5bd97e169d374fd295c057abafd
lmxyjy <yjy15680489038@163.com>
2020-09-19 16:42:44 +0800

    通用的单利模式

--- 单例模式/one.js
+++ 单例模式/one.js
@@ -18,4 +18,48 @@ var one;
         };
         return Singleton;
     }());
+    var myApp = {};
+    myApp.namespace = function (name) {
+        var parts = name.split(".");
+        var current = myApp;
+        for (var i in parts) {
+            if (!current[parts[i]]) {
+                current[parts[i]] = {};
+            }
+            current = current[parts[i]];
+        }
+    };
+    //使用闭包封装私有变量
+    var user = (function () {
+        var _name = 'sven', _age = 29;
+        return {
+            getUserInfo: function () {
+                return _name + _age;
+            }
+        };
+    })();
+    //惰性单例
+    var SingleTon = /** @class */ (function () {
+        function SingleTon() {
+        }
+        SingleTon.prototype.getInstance = function () {
+            var instance = null;
+            if (instance == void 0) {
+                instance = new SingleTon();
+            }
+            return instance;
+        };
+        return SingleTon;
+    }());
+    //通用的惰性单例
+    var f = function (fn) {
+        var result;
+        return function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            return result || (result = fn.apply(args));
+        };
+    };
 })(one || (one = {}));
--- 单例模式/one.ts
+++ 单例模式/one.ts
@@ -20,5 +20,53 @@ namespace one{
 
     }
 
-    
+    //动态创建命令空间
+    interface interNamespace {
+        [propname:string]:any
+    }
+    const myApp:interNamespace = {}
+    myApp.namespace = function(name:any){
+        const parts = name.split(".");
+        let current = myApp;
+        for(let i in parts){
+            if(!current[parts[i]]){
+                current[parts[i]] = {}
+            }
+
+            current = current[parts[i]];
+        }
+    }
+
+
+    //使用闭包封装私有变量
+    const user = (function(){
+        var _name = 'sven',
+            _age = 29;
+
+        return {
+            getUserInfo:function(){
+                return _name + _age
+            }
+        }
+    })()
+
+    //惰性单例
+    class SingleTon{
+        getInstance(){
+            let instance:null | SingleTon = null;
+            if(instance == void 0){
+                instance = new SingleTon()
+            }
+            return instance;
+        }
+    }
+
+    //通用的惰性单例
+    var f = function(fn:Function){
+        var result:any
+        return (...args:[])=>{
+            return result || (result = fn.apply(args))
+        }
+
+    }
 }
